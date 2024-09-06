c7b082804b96455290f7d0e21bd2cff9b3a69dad
eliesjebri <eliesjebri@users.noreply.github.com>
2024-09-06 17:43:38 +0100

    Create app.js

---
+++ containers/hellofromhost_nodejs/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const os = require('os');
+
+const hostname = os.hostname();
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`[V3] Hello from ${hostname}\n`);
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
